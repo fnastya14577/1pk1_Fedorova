54845c8f2dc8b08b9a99fc52cee2488a1b7a9945
fnastya14577 <fedorova.nastya.08@mail.ru>
2025-01-24 00:05:02 +0500

    выполнено задание 9 урока 3

---
+++ Task_03_09/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_03_09
+{
+    internal class Program
+    {
+        //через сколько лет вклад составит y рублей//
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите начальную сумму вклада: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите процентную ставку: ");
+            int p = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите желаемую сумму: ");
+            int y = int.Parse(Console.ReadLine());
+
+            int years = 0;
+
+            while (x < y)
+            {
+                x += (x * p) / 100;
+                years++;
+            }
+
+            Console.WriteLine($"Количество лет: {years}");
+        }
+    }
+}

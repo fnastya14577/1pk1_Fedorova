926109db3e77cc3ec4fe136bdb54f371aba935c0
fnastya14577 <fedorova.nastya.08@mail.ru>
2025-01-23 23:58:02 +0500

    выполнено задание 7 урока 3

---
+++ Task_03_07/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_03_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double g = 9.8;
+
+            Console.WriteLine("  t        v");
+
+            for (double t = 0; t <= 10; t += 0.5)
+            {
+                double v = g * t;
+                Console.WriteLine($"{t:F1}\t{v:F2}");
+            }
+        }
+    }
+}
---
+++ Task_03_08/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_03_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

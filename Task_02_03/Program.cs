636396aefb3c73d36654c6aab369f12ba0eec3d2
fnastya14577 <fedorova.nastya.08@mail.ru>
2025-01-23 22:55:05 +0500

    выполнено задание 5 урока 3

--- Task_02_03/Program.cs
+++ Task_02_03/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите начало диапазона температуры: ");
+            double startTemp = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите конец диапазона температуры: ");
+            double endTemp = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите шаг изменения температуры: ");
+            double step = double.Parse(Console.ReadLine());
+
+            for (double i = startTemp; i <= endTemp; i += step)
+            {
+                double tempF = i * 1.8 + 32;
+                Console.WriteLine($"Температура в Цельсиях: {i}; Температура в Фаренгейтах: {tempF}");
+            }
         }
     }
 }
--- Task_03_04/Program.cs
+++ Task_03_04/Program.cs
@@ -6,6 +6,21 @@
         static void Main(string[] args)
         {
             /*решение задачи*/
+            int count = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "exit" || input == "")
+                {
+                    Console.WriteLine($"Количество введенных строк пользователем: {count}");
+                    break;
+                }
+
+                Console.Clear();
+                count++;
+            }
         }
     }
 }

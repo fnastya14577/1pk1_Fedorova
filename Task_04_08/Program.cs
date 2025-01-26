62ad6102d2398a77f16df818f221d43669d85ba9
fnastya14577 <fedorova.nastya.08@mail.ru>
2025-01-26 12:04:15 +0500

    выполнено задание 8 урока 4

---
+++ Task_04_08/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task_04_08
+{
+    internal class Program
+    {
+        //Дан массив, содержащий последовательность 50 случайных чисел.Найти количество пар элементов, значения которых равны.
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[50];
+            Random random = new Random();
+
+            for (int index = 0; index < numbers.Length; index++)
+            {
+                numbers[index] = random.Next(0, 49);
+                Console.Write(numbers[index] + " ");
+            }
+
+            //поиск пар 
+            int countOfPairs = 0;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] ==  numbers[i - 1])
+                {
+      
+                    countOfPairs++;
+                }
+            }
+            Console.WriteLine($"Количество пар: {countOfPairs}");
+        }
+    }
+}
+
+        
+    
+

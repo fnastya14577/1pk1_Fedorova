e1d56368d38ebe25455e8d0f40257c907242a79e
fnastya14577 <fedorova.nastya.08@mail.ru>
2025-01-26 15:19:07 +0500

    выполнено задание 8 урока 3

--- Task_03_08/Program.cs
+++ Task_03_08/Program.cs
@@ -2,9 +2,24 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //Даны натуральные числа от 20 до 50. Напечатать те из них, которые делятся на 3, но не делятся на 5.
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int startNumber = 20;
+            int endNumber = 50;
+
+            Console.WriteLine("Натуральные числа от 20 до 50, которые делятся на 3, но не делятся на 5:");
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("\nНажмите любую клавишу для выхода.");
+            Console.ReadKey();
         }
     }
 }

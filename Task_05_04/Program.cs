d1d372b2e74fe7fb583ab6a59ff5a55024012499
fnastya14577 <fedorova.nastya.08@mail.ru>
2025-01-30 22:50:48 +0500

    выполнено задание 4 урока 5

---
+++ Task_05_04/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Task_05_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Дан квадратный массив размерность n*n. Произведите анализ данной матрицы и выясните является ли данная матрица
+            диагональной (все элементы вне главной диагонали равны нулю)
+            Если матрица является диагональной, то вывести ее повторно с цветовым выделением главной диагонали. Если нет, то вывеси
+            сообщение что матрица не является диагональной. */
+
+            int[,] matrix =
+            {
+                { 1, 0, 0 },
+                { 0, 5, 0 },
+                { 0, 0, 7 }
+            };
+
+            bool isDiagonal = true;
+            int n = matrix.GetLength(0);
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j && matrix[i, j] != 0)
+                    {
+                        isDiagonal = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isDiagonal)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (i == j)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(matrix[i, j] + " ");
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.Write(matrix[i, j] + " ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Матрица не является диагональной");
+            }
+        }
+    }
+}
+

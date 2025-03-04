d9d8aaa297eaf599f6ef939d1b34a810098941b0
fnastya14577 <fedorova.nastya.08@mail.ru>
2025-03-04 22:27:26 +0500

    выполнено задание 6 урока 11

---
+++ Task_11_06/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_11_06
+{
+    internal class Program
+    {
+        /*Напишите метод, который принимает массив целых чисел и
+          изменяет его элементы, увеличивая каждый на 1. Проверьте, изменился ли оригинальный
+          массив вне метода.*/
+        static void Main(string[] args)
+        {
+            int[] Array1 = { 1, 2, 3, 4, 5 };
+
+            // Вывод массива
+            Console.WriteLine("Оригинальный массив: " + string.Join(", ", Array1));
+            Array2(Array1);
+
+            // Вывод оригинального массива после вызова метода
+            Console.WriteLine("Оригинальный массив после вызова метода: " + string.Join(", ", Array1));
+        }
+        static void Array2(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] += 1; // Увеличиваем каждый элемент на 1
+            }
+        }
+    }
+}

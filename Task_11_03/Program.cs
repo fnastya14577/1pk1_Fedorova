f4b5f98e20b702dea698182b0e30524c40cd2776
fnastya14577 <fedorova.nastya.08@mail.ru>
2025-03-04 21:54:06 +0500

    выполнено задание 3 урока 11

---
+++ Task_11_03/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task_11_03
+{
+    internal class Program
+    {
+        /*Напишите метод, который принимает строку и возвращает через
+          выходной параметр количество гласных и согласных букв в этой строке*/
+        static void Main(string[] args)
+        {
+            string input = "Пример строки для анализа";
+            CountLetters(input, out int Count1, out int Count2);
+
+            Console.WriteLine($"Гласные: {Count1}, Согласные: {Count2}");
+        }
+
+        static void CountLetters(string input, out int vowels, out int consonants)
+        {
+            vowels = 0;
+            consonants = 0;
+            string vowelsList = "аеёиоуыэюяАЕЁИОУЫЭЮЯ";
+
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (vowelsList.Contains(c))
+                    {
+                        vowels++;
+                    }
+                    else
+                    {
+                        consonants++;
+                    }
+                }
+            }
+        }
+    }
+}
+    
+

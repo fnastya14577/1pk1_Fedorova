06f5671610c90136e664f26511e125bf64b20799
fnastya14577 <fedorova.nastya.08@mail.ru>
2025-03-04 23:44:19 +0500

    выполнено задание 8 урока 11

---
+++ Task_11_08/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_11_08
+{
+    internal class Program
+    {
+        /*Напишите метод, который принимает переменное количество
+          чисел и возвращает их сумму и максимальное значение через выходные параметры (out).*/
+        static void Main(string[] args)
+        {
+            // Вызов метода с переменным количеством аргументов
+            SumAndMax(out double max, out double sum, 1, 2, 3, 4, 5);
+            Console.WriteLine($"Сумма: {sum}, Максимальное значение: {max}");
+        }
+        static void SumAndMax(out double max, out double sum, params int[] numbers)
+        {
+            sum = 0;
+            max = int.MinValue; // Инициализация максимального значения
+
+            foreach (var number in numbers)
+            {
+                sum += number;
+                if (number > max)
+                {
+                    max = number; // Обновление максимального значения
+                }
+            }
+        }
+    }
+}

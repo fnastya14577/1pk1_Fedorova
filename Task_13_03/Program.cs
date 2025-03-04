c6d338400a19c4d905f0949b133feee618099cc2
fnastya14577 <fedorova.nastya.08@mail.ru>
2025-03-05 00:27:14 +0500

    выполнено задание 3 урока 13

--- Task_13_03/Program.cs
+++ Task_13_03/Program.cs
@@ -2,26 +2,57 @@
 
 namespace Task_13_03
 {
-    public class Car 
+
+    internal class Program
     {
-        public string Number { get; set; }
-        public string CarBrand { get; set; }
-        public string Color { get; set; }
-        public double Speed { get; set; }
-
-        public Car(string number, string carBrand, string color, double speed)
+        static void Main(string[] args)
         {
-            Number = number;
-            CarBrand = carBrand;
-            Color = color;
-            Speed = speed;
-        }
-        public Car(string number, string carBrand)
-        {
-            Number = number;
-            CarBrand = carBrand;
+            Console.WriteLine();
         }
 
-    }
-    
+        public class Car
+        {
+            public string Number { get; set; }
+            public string CarBrand { get; set; }
+            public string Color { get; set; }
+            public double Speed { get; set; }
+
+            public Car(string number, string carBrand, string color, double speed)
+            {
+                Number = number;
+                CarBrand = carBrand;
+                Color = color;
+                Speed = speed;
+            }
+            public void Accelerate(int increase)
+            {
+                Speed += increase;
+                Console.WriteLine($"Автомобиль {CarBrand} разогнался до {Speed} км/ч.");
+
+            }
+            // Метод для торможения автомобиля
+            public void Brake(int maxSpeed)
+            {
+                if (Speed > maxSpeed)
+                {
+                    Console.WriteLine($"Превышение скорости! {CarBrand} останавливается.");
+                    Speed = 0;
+                }
+                else
+                {
+                    Console.WriteLine($"{CarBrand} замедляется.");
+                }
+            }
+        }
+            public static void Main()
+            {
+                Car car1 = new CarBrand("A123АС", "Toyota", "Зеленый");
+                car1.Accelerate(100); 
+                car1.Brake(60);      
+
+                Car car2 = new CarBrand("A153АС", "Toyota", "Красный");
+                car1.Accelerate(70);
+                car1.Brake(40);
+            }
+     }
 }

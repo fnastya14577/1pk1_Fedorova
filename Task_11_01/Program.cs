68650fce85d6acb2e6c05cc82323196114d01d47
fnastya14577 <fedorova.nastya.08@mail.ru>
2025-03-04 21:28:51 +0500

    done

---
+++ Task_11_01/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task_11_01
+{
+    internal class Program
+    {
+        /*Передача по значению: Напишите метод, который принимает два целых числа и меняет их
+         местами. Проверьте, изменились ли значения переменных вне метода*/
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+
+            Console.WriteLine($"До вызова метода: x = { x}, y = { y}");
+            Swap(x, y);
+            Console.WriteLine($"После вызова метода: x = {x}, y = {y}");           
+        }
+        static void Swap(int a, int b)
+        { 
+        int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
---
+++ Task_13_03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Linq;
+
+namespace Task_13_03
+{
+    public class Car 
+    {
+        public string Number { get; set; }
+        public string CarBrand { get; set; }
+        public string Color { get; set; }
+        public double Speed { get; set; }
+
+        public Car(string number, string carBrand, string color, double speed)
+        {
+            Number = number;
+            CarBrand = carBrand;
+            Color = color;
+            Speed = speed;
+        }
+        public Car(string number, string carBrand)
+        {
+            Number = number;
+            CarBrand = carBrand;
+        }
+
+    }
+    
+}

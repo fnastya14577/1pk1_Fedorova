170f2b5ec408e3b8d5042fb94c0e9498c9be5260
fedorova.nastya.08@mail.ru <fedorova.nastya.08@mail.ru>
2025-03-31 23:26:44 +0500

    done

---
+++ Task_19_02/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task_19_02
+{
+    internal class Program
+    {
+        /* Напишите программу, которая запрашивает у пользователя произвольный текст, 
+         * содержащий предложения (есть знаки препинания). программу после анализа выводит текст, 
+         * разделенный на части:
+         a)	По пробелам (отдельные слова построчно)
+         b)	По предложениям (отдельные предложения построчно)
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите произвольный текст:");
+            string inputText = Console.ReadLine();
+
+            string[] words = inputText.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("Текст, разделённый на отдельные слова:");
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            char[] delimiters = { ' ', '.', ',', ';', ':', '!', '?' };
+            string[] phrases = inputText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("Текст, разделённый на отдельные предложения:");
+            foreach (var phrase in phrases)
+            {
+                Console.WriteLine(phrase);
+            }
+        }
+    }
+}

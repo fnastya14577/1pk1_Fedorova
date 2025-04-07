c2ee9d2d2cf523545b64e13309372bc4b51efcdd
fedorova.nastya.08@mail.ru <fedorova.nastya.08@mail.ru>
2025-04-07 21:38:17 +0500

    done

---
+++ Task_20_04/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_20_04
+{
+    internal class Program
+    {
+        /* Создайте программу для учета транспортных средств. Используйте перечисление VehicleType:
+           • Car
+           • Bike
+           • Bus
+           • Truck
+           • Motorcycle
+          Храните список транспортных средств (можно просто List<VehicleType>). 
+          Добавьте метод для подсчёта транспорта определённого типа. 
+          Реализуйте поиск по типу и вывод информации. */
+        static void Main(string[] args)
+        {
+            VehicleInventory inventory = new VehicleInventory();
+
+            inventory.AddVehicle(VehicleType.Car, "Toyota Camry");
+            inventory.AddVehicle(VehicleType.Bike, "Giant");
+            inventory.AddVehicle(VehicleType.Bus, "Mercedes");
+            inventory.AddVehicle(VehicleType.Truck, "Volvo FH");
+            inventory.AddVehicle(VehicleType.Motorcycle, "Yamaha R1");
+            inventory.AddVehicle(VehicleType.Car, "Honda Accord");
+
+            inventory.DisplayAllVehicles();
+
+            int carCount = inventory.CountVehiclesByType(VehicleType.Car);
+            Console.WriteLine($"\nКоличество автомобилей: {carCount}");
+        }
+    }
+}
---
+++ Task_20_04/Vehicle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20_04
+{
+    public enum VehicleType
+    {
+        Car,        
+        Bike,       
+        Bus,        
+        Truck,      
+        Motorcycle   
+    }
+
+    public class Vehicle
+    {
+        public VehicleType Type { get; private set; }
+        public string Model { get; private set; }
+
+        public Vehicle(VehicleType type, string model)
+        {
+            Type = type;
+            Model = model;
+        }
+
+        public override string ToString() => $"Тип: {Type}, Модель: {Model}";
+    }
+
+    public class VehicleInventory
+    {
+        private List<Vehicle> _vehicles;
+
+        public VehicleInventory()
+        {
+            _vehicles = new List<Vehicle>();
+        }
+
+        public void AddVehicle(VehicleType type, string model)
+        {
+            _vehicles.Add(new Vehicle(type, model));
+            Console.WriteLine($"Добавлено: {type}, Модель: {model}");
+        }
+
+        public int CountVehiclesByType(VehicleType type)
+        {
+            return _vehicles.Count(v => v.Type == type);
+        }
+
+        public void DisplayAllVehicles()
+        {
+            Console.WriteLine("Список всех транспортных средств:");
+            foreach (var vehicle in _vehicles)
+            {
+                Console.WriteLine(vehicle);
+            }
+        }
+    }
+}

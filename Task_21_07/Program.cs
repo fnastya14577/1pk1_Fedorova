f94108db059ca957aadc5b7595e835a3b19b4d08
fedorova.nastya.08@mail.ru <fedorova.nastya.08@mail.ru>
2025-04-07 22:38:51 +0500

    done

---
+++ Task_21_07/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_21_07
+{
+    internal class Program
+    {
+        /* Написать метод, который находит первый ключ в словаре, 
+         * соответствующий заданному значению. 
+         * Если значения нет, вернуть null. */
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, int>
+        {
+            { "item1", 10 },
+            { "item2", 30 },
+            { "item3", 40 },
+            { "item4", 10 }
+        };
+
+            string key = FindKey(dict, 10);
+            Console.WriteLine(key ?? "null");
+
+            key = FindKey(dict, 40);
+            Console.WriteLine(key ?? "null");
+        }
+        static string FindKey(Dictionary<string, int> dictionary, int value)
+        {
+            foreach (var i in dictionary)
+            {
+                if (i.Value.Equals(value))
+                {
+                    return i.Key;
+                }
+            }
+            return null;
+        }
+    }
+}

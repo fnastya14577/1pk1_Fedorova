bc75aaf7430e56f25f10b10f347272c531082567
fedorova.nastya.08@mail.ru <fedorova.nastya.08@mail.ru>
2025-04-07 22:43:17 +0500

    done

---
+++ Task_21_08/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task_21_08
+{
+    internal class Program
+    {
+        /* напишите метод, 
+         * который на вход получается массив параметров (строк)
+         * и возвращает толькоуникальныестроки. */
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, int>
+        {
+            {"first", 1},
+            {"second", 2},
+            {"third", 3},
+            {"fourth", 2}
+        };
+            var valueToFind = 2;
+            var key = FindKeyByValue(dictionary, valueToFind);
+
+            if (key != null)
+            {
+                Console.WriteLine($"Ключ, соответствующий значению {valueToFind}: {key}");
+            }
+            else
+            {
+                Console.WriteLine($"Значение {valueToFind} не найдено в словаре.");
+            }
+        }
+        public static string FindKeyByValue(Dictionary<string, int> dictionary, int value)
+        {
+            foreach (var pair in dictionary)
+            {
+                if (pair.Value.Equals(value))
+                {
+                    return pair.Key; 
+                }
+            }
+            return null; 
+        }
+    }
+}

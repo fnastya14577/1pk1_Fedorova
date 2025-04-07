d924808d51ec8c66b23c4e0914d85148bd2a76a6
fedorova.nastya.08@mail.ru <fedorova.nastya.08@mail.ru>
2025-04-07 22:20:24 +0500

    done

---
+++ Task_21_06/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task_21_06
+{
+    internal class Program
+    {
+        /* Написать метод, который удаляет повторяющиеся элементы из списка, 
+         * сохраняя порядок. */
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 2, 4, 3, 5, 1, 6 };
+            List<int> uniqueNumbers = RemoveDuplicates(numbers);
+
+            Console.WriteLine("Список с уникальными элементами:");
+            foreach (var number in uniqueNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public static List<T> RemoveDuplicates<T>(List<T> inputList)
+        {
+            HashSet<T> seen = new HashSet<T>();
+            List<T> resultList = new List<T>();
+
+            foreach (var item in inputList)
+            {
+                if (seen.Add(item))
+                {
+                    resultList.Add(item);
+                }
+            }
+            return resultList;
+        }
+    }
+}

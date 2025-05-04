45f9025a592f44f6b498d31905744c1245a96ee9
fedorova.nastya.08@mail.ru <fedorova.nastya.08@mail.ru>
2025-05-04 19:08:06 +0500

    done

---
+++ Task_24_07/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_24_07
+{
+    internal class Program
+    {
+        /* Реализуйте функцию, которая ищет заданное слово в текстовом файле 
+           и возвращает все строки, содержащиеэтослово (регистронезависимо). */
+        static void Main(string[] args)
+        {
+            string path = "text.txt";
+            string word = "опыт";
+
+            List<string> linesWithWord = FindWord(path, word);
+
+            foreach (string line in linesWithWord)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        static List<string> FindWord(string path, string word)
+        {
+            List<string> result = new List<string>();
+
+            foreach (string line in File.ReadLines(path))
+            {
+                result.Add(line);
+            }
+            return result;
+        }
+    }
+}

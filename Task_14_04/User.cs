1a2669e2bf1b71a64f38fbbb5a4a50d06ba51949
fedorova.nastya.08@mail.ru <fedorova.nastya.08@mail.ru>
2025-05-30 22:20:15 +0500

    done

---
+++ Task_14_04/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14_04
+{
+    internal class User
+    {
+        private static User currentUser;
+        public static User CurrentUser
+        {
+            get { return currentUser; }
+        }
+        public static void SetCurrentUser(User user)
+        {
+            currentUser = user;
+        }
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public User(string name, int id)
+        {
+            Name = name;
+            ID = id;
+        }
+    }
+}
